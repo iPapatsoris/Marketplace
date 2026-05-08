00bca4f68bb5010e10fce59c0bc071d9b8aeef40
Ioannis <giannhs95@yahoo.gr>
2026-05-08 20:31:40 +0300

    Rename "user" endpoint to "users"

--- src/main/java/com/expense_tracker/user/UserController.java
+++ src/main/java/com/expense_tracker/user/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository repository;
     private final UserMapper mapper;
