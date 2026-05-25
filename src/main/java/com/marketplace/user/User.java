f2708501eec05f39c73b785d465f837779fc5985
Ioannis <giannhs95@yahoo.gr>
2026-05-25 14:07:30 +0300

    Remove redundant @GeneratedValue default param declaration

--- src/main/java/com/marketplace/user/User.java
+++ src/main/java/com/marketplace/user/User.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue
     private Long id;
     private String name;
     private String email;
