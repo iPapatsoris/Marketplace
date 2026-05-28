268e59686da6a1b525865ab866d2b6cfac273e1d
Ioannis <giannhs95@yahoo.gr>
2026-05-28 20:37:01 +0300

    Use Object.equals for Long comparison

--- src/main/java/com/marketplace/product/ProductService.java
+++ src/main/java/com/marketplace/product/ProductService.java
@@ -10,6 +10,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -25,8 +27,8 @@ public class ProductService {
 
     @Transactional
     public UpdateProductResponse updateProduct(Long id, UpdateProductRequest updateProductRequest) {
-       var product = productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException("Product with id %d does not exist".formatted(id)));
-        if (product.version !=  updateProductRequest.version()) {
+       var product = productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+        if (!Objects.equals(product.version, updateProductRequest.version())) {
             throw new OptimisticLockException("Product with id %d has changed".formatted(id));
         }
        var updatedProduct = productMapper.updateProduct(updateProductRequest, product);
