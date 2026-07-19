793295b275adbf0130869c6f80374f026418c57f
Ioannis <giannhs95@yahoo.gr>
2026-07-19 19:45:03 +0300

    Add ReservationService tests

---
+++ src/test/java/com/marketplace/product/ProductBuilder.java
@@ -0,0 +1,41 @@
+package com.marketplace.product;
+
+import java.math.BigDecimal;
+
+public class ProductBuilder {
+    private Long id = 1L;
+    private Long version = 0L;
+    private String name = "chair";
+    private BigDecimal price = new BigDecimal(40);
+    private int inventory = 10;
+
+    public static ProductBuilder aProduct() {
+        return new ProductBuilder();
+    }
+
+    public ProductBuilder withId(Long id) {
+       this.id = id;
+       return this;
+    }
+
+    public ProductBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ProductBuilder withPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public Product build() {
+        Product product = new Product();
+        product.setId(id);
+        product.setVersion(version);
+        product.setName(name);
+        product.setPrice(price);
+        product.setInventory(inventory);
+
+        return product;
+    }
+}
---
+++ src/test/java/com/marketplace/reservation/ReservationServiceTest.java
@@ -0,0 +1,193 @@
+package com.marketplace.reservation;
+
+import com.marketplace.product.Product;
+import com.marketplace.product.ProductBuilder;
+import com.marketplace.product.ProductSnapshot;
+import com.marketplace.product.exception.ProductNotFoundException;
+import com.marketplace.product.repository.ProductRepository;
+import com.marketplace.reservation.dto.CreateReservationRequest;
+import com.marketplace.reservation.dto.CreateReservationResponse;
+import com.marketplace.reservation.dto.ReservationResponse;
+import com.marketplace.reservation.entity.Reservation;
+import com.marketplace.reservation.entity.ReservationBuilder;
+import com.marketplace.reservation.entity.ReservationFactory;
+import com.marketplace.reservation.exception.InsufficientStockException;
+import com.marketplace.reservation.exception.ReservationNotFoundException;
+import jakarta.persistence.OptimisticLockException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tools.jackson.databind.ObjectMapper;
+
+import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+
+import static java.time.temporal.ChronoUnit.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ReservationServiceTest {
+
+    private ReservationService reservationService;
+    private final Clock fixedClock = Clock.fixed(
+            Instant.parse("2026-01-01T12:00:00Z"), ZoneOffset.UTC);
+
+    @Mock
+    private ReservationRepository reservationRepository;
+
+    @Mock
+    private ProductRepository productRepository;
+
+    private final ReservationMapper reservationMapper = Mappers.getMapper(ReservationMapper.class);
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ReservationFactory reservationFactory = new ReservationFactory(fixedClock, reservationMapper);
+
+    @BeforeEach
+    void setup() {
+        reservationService = new ReservationService(reservationMapper, reservationRepository, productRepository, objectMapper, reservationFactory);
+    }
+
+    @Nested
+    class CreateReservationTest {
+        private final Long productId = 1L;
+        private final int quantityRequest = 5;
+        private final Long productVersionRequest = 10L;
+
+        @Test
+        void shouldCreateReservation() {
+            CreateReservationRequest reservationRequest = new CreateReservationRequest(
+                    quantityRequest, productVersionRequest
+            );
+
+            Long newReservationId = 30L;
+            CreateReservationResponse expectedResponse = new CreateReservationResponse(
+                    newReservationId, quantityRequest, fixedClock.instant().plus(10, MINUTES)
+            );
+
+            String productName = "chair";
+            BigDecimal productPrice = new BigDecimal(35);
+            ProductBuilder productBuilder = new ProductBuilder();
+             Product  product = productBuilder
+                     .withId(productId)
+                     .withName(productName)
+                     .withPrice(productPrice).build();
+
+            ProductSnapshot productSnapshot = new ProductSnapshot(productName, productPrice);
+
+            when(productRepository.reserveInventory(
+                    productId, productVersionRequest, quantityRequest
+            )).thenReturn(Optional.of(productSnapshot));
+
+            when(productRepository.getReferenceById(productId)).thenReturn(product);
+
+            when(reservationRepository.save(any())).thenAnswer(
+                    invocationOnMock -> {
+                        Reservation reservation = invocationOnMock.getArgument(0);
+                        reservation.setId(newReservationId);
+                        return reservation;
+                    }
+            );
+
+            CreateReservationResponse response = reservationService.createReservation(productId, reservationRequest);
+            assertEquals(expectedResponse, response);
+            verify(reservationRepository).save(any(Reservation.class));
+        }
+
+        @Test
+        void shouldThrowProductNotFoundExceptionWhenProductDoesNotExist() {
+            CreateReservationRequest reservationRequest = new CreateReservationRequest(
+                    quantityRequest, productVersionRequest
+            );
+
+            when(productRepository.reserveInventory(productId, productVersionRequest, quantityRequest))
+                    .thenReturn(Optional.empty());
+            when(productRepository.existsById(productId))
+                    .thenReturn(false);
+
+            assertThrowsExactly(ProductNotFoundException.class, () ->
+                    reservationService.createReservation(productId, reservationRequest));
+        }
+
+        @Test
+        void shouldThrowOptimisticLockExceptionWhenProductVersionMismatch() {
+            CreateReservationRequest reservationRequest = new CreateReservationRequest(
+                    quantityRequest, productVersionRequest
+            );
+
+            when(productRepository.reserveInventory(productId, productVersionRequest, quantityRequest))
+                    .thenReturn(Optional.empty());
+            when(productRepository.existsById(productId))
+                    .thenReturn(true);
+            when(productRepository.existsByIdAndVersion(productId, productVersionRequest))
+                    .thenReturn(false);
+
+            assertThrowsExactly(OptimisticLockException.class, () ->
+                    reservationService.createReservation(productId, reservationRequest));
+
+        }
+
+        @Test
+        void shouldThrowInsufficientStockExceptionWhenQuantityExceedsStock() {
+            CreateReservationRequest reservationRequest = new CreateReservationRequest(
+                    quantityRequest, productVersionRequest
+            );
+
+            when(productRepository.reserveInventory(productId, productVersionRequest, quantityRequest))
+                    .thenReturn(Optional.empty());
+            when(productRepository.existsById(productId))
+                    .thenReturn(true);
+            when(productRepository.existsByIdAndVersion(productId, productVersionRequest))
+                    .thenReturn(true);
+
+            assertThrowsExactly(InsufficientStockException.class, () ->
+                    reservationService.createReservation(productId, reservationRequest));
+
+        }
+    }
+
+    @Nested
+    class GetReservationTest {
+
+        @Test
+        void shouldReturnReservationWhenItExists() {
+            Long id = 1L;
+            ReservationBuilder reservationBuilder =
+                    new ReservationBuilder()
+                            .withId(id)
+                            .withOrderId(5L);
+            Reservation storedReservation = reservationBuilder.build();
+
+            ReservationResponse expectedResponse = new ReservationResponse(
+                    storedReservation.getId(), storedReservation.getStatus(), storedReservation.getOrderId());
+
+            when(reservationRepository.findById(id))
+                    .thenReturn(Optional.of(storedReservation));
+
+            ReservationResponse result = reservationService.getReservation(id);
+
+            assertEquals(expectedResponse, result);
+        }
+
+        @Test
+        void shouldThrowReservationNotFoundExceptionWhenReservationDoesNotExist() {
+            Long id = 1L;
+
+            when(reservationRepository.findById(id))
+                    .thenReturn(Optional.empty());
+
+            assertThrowsExactly(ReservationNotFoundException.class, () -> reservationService.getReservation(id));
+        }
+
+    }
+
+}
