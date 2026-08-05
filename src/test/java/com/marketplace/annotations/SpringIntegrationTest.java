efc31ae32664342ec03946650bda768444dfb7e4
Ioannis <giannhs95@yahoo.gr>
2026-08-05 14:54:39 +0300

    Add annotation for @SpringBootTest configuration

---
+++ src/test/java/com/marketplace/annotations/SpringIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.marketplace.annotations;
+
+import com.marketplace.config.TestcontainersConfiguration;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(TYPE)
+@Retention(RUNTIME)
+@SpringBootTest
+@AutoConfigureRestTestClient
+@Import(TestcontainersConfiguration.class)
+public @interface SpringIntegrationTest {
+}
